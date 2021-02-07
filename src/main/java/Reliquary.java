d077200fed752d864c54e9b770ef4032b5af9374
Tom Quinn <thomashquinn@gmail.com>
2021-02-07 11:44:52 -0500

    localization fixes

--- src/main/java/Reliquary.java
+++ src/main/java/Reliquary.java
@@ -14,6 +14,7 @@ import com.megacrit.cardcrawl.localization.*;
 import com.megacrit.cardcrawl.unlock.UnlockTracker;
 import powers.*;
 import relics.*;
+import util.ReliquaryLogger;
 
 import java.nio.charset.StandardCharsets;
 
